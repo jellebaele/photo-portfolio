2891d5842d5617c95e85edb389f1b9d04feb4ac9
Jelle Baele <jellebaele@hotmail.com>
2020-11-18 17:54:06 +0100

    finished design home.html

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,3 @@ const tl = gsap.timeline({defaults: {
 tl.to('.slider', {opacity:0, duration:3});
 tl.fromTo('.main-text', {opacity: 0}, {opacity: 1, duration: 1.5}, '-=1');
 tl.to('.view-site', {y:'0%', duration: 1.5}, '-=1.5');
-/*
-tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
-tl.to(".slider", {y: '-100%', duration: 1.5, delay: 0.5});
-tl.to('.intro', {y:'-100%', duration: 1}, '-=1');
-tl.fromTo('.main-text', {opacity: 0}, {opacity: 1, duration: 1});*/
